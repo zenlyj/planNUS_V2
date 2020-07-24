4751103d40cdd374c3c2c997be99211f5b1ba22a
Marcus Lim <lim_marcus_97@hotmail.com>
2020-07-24 18:35:20 +0800

    [Backend] added db schema for imports

---
+++ backend/plannus.sql
@@ -0,0 +1,125 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 24, 2020 at 12:33 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 5.6.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `plannus`
+--
+CREATE DATABASE IF NOT EXISTS `plannus` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `plannus`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `diary`
+--
+
+DROP TABLE IF EXISTS `diary`;
+CREATE TABLE `diary` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `note` varchar(255) NOT NULL,
+  `nusnet` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+DROP TABLE IF EXISTS `task`;
+CREATE TABLE `task` (
+  `nusnet` varchar(10) NOT NULL,
+  `id` varchar(10) NOT NULL,
+  `taskPresent` tinyint(1) NOT NULL,
+  `taskName` varchar(100) DEFAULT NULL,
+  `module` varchar(100) DEFAULT NULL,
+  `timeFrom` int(5) NOT NULL,
+  `timeTo` int(5) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `idpk` int(11) NOT NULL,
+  `week` int(2) NOT NULL,
+  `completed` tinyint(1) NOT NULL DEFAULT '0',
+  `diaryID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nusnet` varchar(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `hash` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `diary`
+--
+ALTER TABLE `diary`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`idpk`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nusnet` (`nusnet`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `diary`
+--
+ALTER TABLE `diary`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT for table `task`
+--
+ALTER TABLE `task`
+  MODIFY `idpk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1435;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=93;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
