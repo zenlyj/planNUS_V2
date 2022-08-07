530d7d271efdc1ff4d68c2f9843e8593a86e7111
Zen Lee <iarezenly@gmail.com>
2022-08-07 13:17:16 +0800

    Initialize spring boot backend

---
+++ backend_springboot/src/main/java/com/orbital/planNUS/PlanNusApplication.java
@@ -0,0 +1,13 @@
+package com.orbital.planNUS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanNusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanNusApplication.class, args);
+	}
+
+}
---
+++ backend_springboot/src/test/java/com/orbital/planNUS/PlanNusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orbital.planNUS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanNusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
