6d55c720a9ca96d524990b7a3bfa00891824babd
Zen Lee <iarezenly@gmail.com>
2022-08-08 20:12:06 +0800

    Refactor Task creation dialog

--- plannus/src/components/Task.js
+++ plannus/src/components/Task.js
@@ -1,51 +1,149 @@
-import React, { Component } from 'react'
-import TaskInput from './TaskInput'
+import React from 'react'
+import Dialog from '@mui/material/Dialog'
+import DialogTitle from '@mui/material/DialogTitle'
+import DialogContent from '@mui/material/DialogContent'
+import DialogActions from '@mui/material/DialogActions'
+import { Button } from '@mui/material'
+import { styled } from '@mui/material/styles'
+import IconButton from '@mui/material/IconButton'
+import CloseIcon from '@mui/icons-material/Close'
+import TextField from '@mui/material/TextField'
+import PropTypes from 'prop-types'
 
-class Task extends Component {
-    constructor(props) {
-        super(props);
-        this.state = this.props.initTask
-        this.updateTask = this.updateTask.bind(this)
-        this.markTaskCompleted = this.markTaskCompleted.bind(this)
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    '& .MuiDialogContent-root': {
+      padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+      padding: theme.spacing(1),
+    },
+  }))
+
+const BootstrapDialogTitle = (props) => {
+    const { children, onClose, ...other } = props
+  
+    return (
+      <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+        {children}
+        {onClose ? (
+          <IconButton
+            aria-label="close"
+            onClick={onClose}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        ) : null}
+      </DialogTitle>
+    )
+}
+
+BootstrapDialogTitle.propTypes = {
+    children: PropTypes.node,
+    onClose: PropTypes.func.isRequired,
+}
+
+function Task(props) {
+    const [open, setOpen] = React.useState(false)
+    const [hovered, setHovered] = React.useState(false)
+    
+    const handleClickOpen = () => {
+        setOpen(true)
     }
 
-    componentDidUpdate(prevProps) {
-        if ((prevProps.initTask.taskPresent !== this.props.initTask.taskPresent) || 
-            (prevProps.initTask.taskName !== this.props.initTask.taskName) || 
-            (prevProps.initTask.module !== this.props.initTask.module) ||
-            (prevProps.initTask.timeTo !== this.props.initTask.timeTo) ||
-            (prevProps.initTask.description !== this.props.initTask.description)
-        ) {
-            this.setState(this.props.initTask)
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    const defaultButton = () => {
+        const defaultButtonStyle = {   
+            background: 'transparent',
+            border: 0,
+            width:'100%',
+            height:'30px',
+            color: '#a2b2d3',
+            fontSize: 20
         }
+        return (
+            <button
+                style={defaultButtonStyle} onMouseOver={() => setHovered(true)} onMouseLeave={() => setHovered(false)} onClick={() => handleClickOpen()}
+            >
+                {hovered ? '+' : null}
+            </button>
+        )
     }
-
-    updateTask(taskPresent, updatedInfo) {
-        let updatedTask = {
-            id: this.state.id,
-            taskPresent: taskPresent,
-            taskName: updatedInfo.taskName,
-            module: updatedInfo.module,
-            timeFrom: updatedInfo.timeFrom,
-            timeTo: updatedInfo.timeTo,
-            description: updatedInfo.description
-        }
-        this.props.updateTable(updatedTask)
-    }
-
-    markTaskCompleted() {
-        this.props.markTaskCompleted(this.state.id)
-    }
-
-    render() {
-        return (<TaskInput
-                    taskInfo={this.state}
-                    updateTask={this.updateTask}
-                    calendarView={this.props.calendarView}
-                    markTaskCompleted={this.markTaskCompleted}
-                    taskCompleted={this.props.taskCompleted}
-                />)
-    } 
+    
+    return (
+        <div>
+            {defaultButton()}
+            <BootstrapDialog
+                onClose={handleClose}
+                aria-labelledby="customized-dialog-title"
+                open={open}
+            >
+                <BootstrapDialogTitle id="customized-dialog-title" onClose={handleClose}>
+                    New Task
+                </BootstrapDialogTitle>
+                <DialogContent dividers>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Task Name"
+                        type="name"
+                        fullWidth
+                        variant="standard"
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="module"
+                        label="Module"
+                        type="module"
+                        fullWidth
+                        variant="standard"
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="timeFrom"
+                        label="From"
+                        type="timeFrom"
+                        fullWidth
+                        variant="standard"
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="timeTo"
+                        label="To"
+                        type="timeTo"
+                        fullWidth
+                        variant="standard"
+                    />
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="description"
+                        label="Task Description"
+                        type="description"
+                        fullWidth
+                        variant="standard"
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>
+                        Save
+                    </Button>
+                </DialogActions>
+            </BootstrapDialog>
+        </div>
+    )
 }
 
 export default Task
